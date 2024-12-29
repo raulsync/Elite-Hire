b5343147fed4030f3b9bc54ebf927ac3b61ff00e
raulsync <rahul.aanand086@gmail.com>
2024-12-29 23:34:56 +0530

    feat:signup-login-logout

---
+++ server/src/controllers/user-controller.ts
@@ -0,0 +1,145 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model";
+
+export const registerUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password, phoneNumber, role } = req.body;
+
+    if (!name || !email || !password || !phoneNumber || !role) {
+      return res.status(404).json({
+        message: "All fields are required",
+        success: false,
+      });
+    }
+
+    //user exists or not
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      res.status(400).json({
+        message: "User already exist with same email or phonenumber",
+        success: false,
+      });
+    }
+
+    //password hash
+
+    const hashPassword = bcrypt.hash(password, 10);
+
+    const newUser = await new User({
+      name,
+      email,
+      password: hashPassword,
+      phoneNumber,
+      role,
+    });
+    newUser.save();
+    res.json({
+      data: newUser,
+      message: "User saved successfully",
+      success: true,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({
+        message: error.message,
+        success: false,
+      });
+    }
+  }
+};
+
+//Login
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      return res.status(401).json({
+        message: "All fields are required",
+        success: false,
+      });
+    }
+
+    //find user
+
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send({
+        message: "user not found",
+        success: false,
+      });
+    }
+
+    //compare password with hashed password
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).send({
+        message: "Incorrect Password",
+        success: false,
+      });
+    }
+
+    user = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      profile: user.profile,
+      role: user.role,
+    };
+
+    //generate token
+    const jwtConfig = {
+      userId: user!._id,
+    };
+
+    const token = await jwt.sign(jwtConfig, process.env.JWT_SECRET_KEY!, {
+      expiresIn: "1d",
+    });
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .send({
+        message: `Welcome ${user?.name}`,
+        user,
+        success: true,
+      });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error in login", error.message);
+      return res.status(501).json({
+        message: error.message,
+        success: false,
+      });
+    }
+  }
+};
+
+//logout
+
+export const logout = async (req: Request, res: Response) => {
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).send({
+      message: "logout successfully",
+      success: true,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error in logout api", error.message);
+      return res.status(501).json({
+        message: error.message,
+        success: false,
+      });
+    }
+  }
+};
--- server/src/models/compony.model.ts
+++ server/src/models/compony.model.ts
@@ -1,29 +1,34 @@
 import mongoose from "mongoose";
 
-const companySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+    },
+    logo: {
+      type: String,
+    },
+    userId: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-  website: {
-    type: String,
-  },
-  logo: {
-    type: String,
-  },
-  userId:[{
-    type:mongoose.Schema.Types.ObjectId;
-    ref:"User",
-    required:true,
-  }]
-},{timestamps:true});
+  { timestamps: true }
+);
 
 export const Company = mongoose.model("Company", companySchema);
