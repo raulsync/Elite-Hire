3d3e239056b1148a8b5eadc02e1467fcc35328bb
raulsync <rahul.aanand086@gmail.com>
2025-01-01 13:40:23 +0530

    feat:all-company-controller

--- server/src/controllers/company-controller.ts
+++ server/src/controllers/company-controller.ts
@@ -1,12 +1,21 @@
 import { Request, Response } from "express";
 import { Company } from "../models/compony.model";
 import { AuthRequest } from "../types";
+import mongoose from "mongoose";
 
 export const registerCompany = async (req: AuthRequest, res: Response) => {
   try {
-    const { name } = req.body;
-    const company = await Company.find({ name });
-    if (company) {
+    const { name, description, website, location } = req.body;
+
+    if (!name || !description || !website || !location) {
+      return res.status(400).send({
+        message: "All fields are required",
+        success: false,
+      });
+    }
+    const existingCompany = await Company.findOne({ name });
+
+    if (existingCompany) {
       return res.status(400).json({
         message: "Company Already Exist",
         success: false,
@@ -14,10 +23,14 @@ export const registerCompany = async (req: AuthRequest, res: Response) => {
     }
 
     const newCompany = await Company.create({
-      name: name,
+      name,
+      description,
+      location,
+      website,
       userId: req.user?._id,
     });
-    return res.status(200).json({
+
+    return res.status(201).json({
       message: "Comapny registered successfully",
       newCompany,
       success: true,
@@ -47,6 +60,10 @@ export const getCompany = async (req: AuthRequest, res: Response) => {
         success: true,
       });
     }
+    res.send({
+      message: "company fetched successfully",
+      company,
+    });
   } catch (error) {
     if (error instanceof Error) {
       return res.status(500).json({
@@ -73,6 +90,50 @@ export const getCompanyById = async (req: AuthRequest, res: Response) => {
   } catch (error) {
     if (error instanceof Error) {
       console.error("Error in getCompanyById");
+      return res.status(500).json({
+        message: error.message,
+        success: false,
+      });
+    }
+  }
+};
+
+//update company detail
+
+export const updateCompany = async (req: AuthRequest, res: Response) => {
+  try {
+    const { name, description, location, website } = req.body;
+
+    // const file = req.files
+    const updatedCompany = { name, description, location, website };
+
+    //validate id
+
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ error: "Invalid company ID format" });
+    }
+
+    const company = await Company.findOneAndUpdate(
+      { _id: req.params.id },
+      updatedCompany,
+      { new: true }
+    );
+
+    if (!company) {
+      return res.status(401).json({
+        message: "Company not found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "company updated successfully",
+      company,
+      success: true,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error in update company", error.message);
       return res.status(500).json({
         message: error.message,
         success: false,
---
+++ server/src/router/company-router.ts
@@ -0,0 +1,20 @@
+import express, { RequestHandler } from "express";
+import { userAuth } from "../middlewares/auth";
+import {
+  getCompany,
+  getCompanyById,
+  registerCompany,
+  updateCompany,
+} from "../controllers/company-controller";
+
+const companyRouter = express.Router();
+
+companyRouter.post("/register", userAuth, registerCompany as RequestHandler);
+
+companyRouter.get("/get", userAuth, getCompany as RequestHandler);
+
+companyRouter.get("/get/:id", userAuth, getCompanyById as RequestHandler);
+
+companyRouter.put("/update/:id", userAuth, updateCompany as RequestHandler);
+
+export default companyRouter;
