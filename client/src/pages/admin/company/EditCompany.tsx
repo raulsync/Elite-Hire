e37c01674bfe8ea6d99f77c79c2e611d374bfa7d
raulsync <rahul.aanand086@gmail.com>
2025-01-19 23:36:48 +0530

    refactor:admin_company

---
+++ client/src/pages/admin/company/EditCompany.tsx
@@ -0,0 +1,11 @@
+import EditCompanyForm from "@/components/admin/EditCompanyForm";
+
+function EditCompany() {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <EditCompanyForm />
+    </div>
+  );
+}
+
+export default EditCompany;
